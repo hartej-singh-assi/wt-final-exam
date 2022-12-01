a022169bd5b619d84fafbae0a621f962fc22006a
hartej-singh-assi <79645328+hartej-singh-assi@users.noreply.github.com>
2022-12-01 13:37:29 +0530

    Delete script.js

--- A001/Q1/2/script.js
+++
@@ -1,7 +0,0 @@
-
-function modechange(){
-doucument.getElementById("Dark Mode").innerhtml = "LightMode";
-doucument.style.body.backgroundcolor = "black";
-doucument.getElementBytd(par).color = "white";
-
-}
